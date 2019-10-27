1f4bc805c2219490b7972c2af98e6c396fb9b150
Anur Bašić <48890281+bashovski@users.noreply.github.com>
2019-10-27 20:28:01 +0100

    Added introduction, datatypes examples to master branch.

---
+++ src/intro/exercise.cpp
@@ -0,0 +1,3 @@
+/**
+ * In order to repeat what you've learned, write a function which will output 'Hello World!' to the console.
+ */
---
+++ src/intro/part2.cpp
@@ -0,0 +1,25 @@
+/**
+ * As you may have noticed, in the intro we have used so much stuff you don't really know what they do, 
+ * hence, we are about to find out and you'll finally know what's happening out-of-the-box.
+*/
+
+#include <iostream> 
+
+/**
+ * 
+ * #include can be listed in a group of preprocessor/precompilation directives.
+ * What does that mean? 
+ * That means it is being executed before the compilation process which converts our little cute code
+ * to a language of ones and zeros that a machine like your computer can easily read and easily understand.
+ * 
+ * As soon as the inputted code is read, it will be converted to a executable by default by a compiler.
+ * #include directive also has a argument, which is in this situation, a 'iostream', which roughly means it is
+ * including a library, or a external file, not written by you into your program.
+ * 
+ * IOStream library consists of many functions and out-of-the-box functionalities that make 
+ * input-output almost a no-brainer.
+ * 
+ * Now, after we have included iostream to our program, we can use it.
+ * In case you haven't included iostream, compiler will throw errors.
+*/
+
